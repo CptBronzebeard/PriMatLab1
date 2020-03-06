84d085d325295aa2651ab19aff4b43ea96869620
cptbronzebeard <omg4itep@gmail.com>
2020-03-06 23:48:40 +0300

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for MathTest
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+import Lib
+
+main :: IO ()
+main = someFunc
